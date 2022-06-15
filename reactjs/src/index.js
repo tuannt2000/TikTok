2b67cb14cd5451edfd9eaf6277a8717b6d78af02
tuannt2000 <79615528+tuannt2000@users.noreply.github.com>
2022-06-15 22:22:34 +0700

    add file global style and scss reactjs

--- reactjs/src/index.js
+++ reactjs/src/index.js
@@ -3,13 +3,16 @@ import { createRoot } from 'react-dom/client';
 import App from '~/App';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
+import GlobalStyles from '~/components/GlobalStyles'
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <App />
+    <GlobalStyles>
+      <App />
+    </GlobalStyles>
   </React.StrictMode>
 );
 
