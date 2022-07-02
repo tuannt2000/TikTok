973a93195ce21be48bca7e8641ae0ac241cae674
tuannt2000 <79615528+tuannt2000@users.noreply.github.com>
2022-07-02 20:29:56 +0700

    custom hook useDebounce

--- reactjs/src/components/Layout/components/Search/index.js
+++ reactjs/src/components/Layout/components/Search/index.js
@@ -9,6 +9,7 @@ import AccountItem from '~/components/AccountItem';
 import { Wrapper as PopperWrapper } from '~/components/Popper';
 import { SearchIcon, ClearSearchIcon } from '~/components/Icons';
 import { useState, useEffect, useRef } from 'react';
+import { useDebounce } from '~/hooks'
 
 const cx = classNames.bind(styles);
 
@@ -18,17 +19,19 @@ function Search() {
     const [showResult, setShowResult] = useState(false);
     const [loading, setLoading] = useState(false);
 
+    const debounced = useDebounce(searchValue, 500);
+
     const inputTextRef = useRef();
 
     useEffect(() => {
-        if(!searchValue.trim()){
+        if(!debounced.trim()){
             setSearchResult([]);
             return;
         }
 
         setLoading(true);
 
-        fetch(`https://tiktok.fullstack.edu.vn/api/users/search?q=${encodeURIComponent(searchValue)}&type=less`)
+        fetch(`https://tiktok.fullstack.edu.vn/api/users/search?q=${encodeURIComponent(debounced)}&type=less`)
             .then(res => res.json())
             .then(res => {
                 setSearchResult(res.data);
@@ -37,7 +40,7 @@ function Search() {
             .catch(() => {
                 setLoading(false);
             })
-    }, [searchValue]);
+    }, [debounced]);
 
     const handleClear = () => {
         setSearchValue('');
